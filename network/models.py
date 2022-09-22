5bb0e2abc9bab69be1153114100fb13fa393499a
ravanton <antonrekhva@gmail.com>
2022-09-22 20:34:13 +0300

    Add class Profile

--- network/models.py
+++ network/models.py
@@ -4,3 +4,7 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    folowers = models.ManyToManyField(User, related_name="get_followed_profiles")
