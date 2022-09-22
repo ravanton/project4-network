3956a0407dffcd230e0b1d5a48558a34c38e0c1d
ravanton <antonrekhva@gmail.com>
2022-09-22 22:12:51 +0300

    function load_posts

--- network/static/network/index.js
+++ network/static/network/index.js
@@ -3,6 +3,28 @@ document.addEventListener("DOMContentLoaded", function(){
     window.history.pushState('The Network', 'The Network', 'http://127.0.0.1:8000/');
     if(document.getElementById('following')) {
         document.getElementById('following').addEventListener('click', () => load_posts ("/followed",1));
-        
+
+    }else {
+        document.getElementById('newPost').addEventListener('click', () => force_login());
+
     }
-})
+    load.posts("",1);
+});
+function load_posts(addon,page){
+    if(addon.includes("?")) {
+        addon += `&page = ${page}`;
+    }else {
+        document.querySelector('#profile').style.display = 'none';
+        addon += `?page = ${page}`;
+    }
+     console.log(`access ${addon}`);
+     fetch(`/load ${addon}`);
+     .then(response => response.jsone())
+     .then(response => {
+        document.getElementById('post').innerHTML ="";
+        build_paginator(addon, page, response.num_pages);
+
+        }
+
+    }
+}
