747034f6fb8cada5acf3fb6def20ba8c022c7d72
ravanton <antonrekhva@gmail.com>
2022-09-23 20:59:47 +0300

    add function show profile

--- network/static/network/index.js
+++ network/static/network/index.js
@@ -33,6 +33,7 @@ function build_paginator(addon, page,num_pages){
     page_list = document.getElementById('paginator');
     page.list.innerHTML = "";
 }
+
 function build_post(post){
     const post_card = document.createElement('div');
     post_card.className = "card col col-6";
@@ -52,4 +53,30 @@ function build_post(post){
     text.id = `content_${post.id}`;
     text.innerHTML = post.content;
     card_body.append(text);
+}
+
+function show_profile(creator_id){
+    load_posts(`?profile=${creator_id}`,1);
+    document.querySelector('#newPost').style.display = 'none';
+    follow_button = document.getElementById('follow-button');
+    follow_button.style.display = 'none';
+    document.querySelector('#profile').style.display = 'block';
+    fetch(`/profile/${creator_id}`)
+    .then(response => response.json())
+    .then(profile => {
+        document.getElementById('following-amount').innerHTML = profile.following;
+        document.getElementById('following-amount').innerHTML = profile.followers;
+        document.getElementById('profile_username').innerHTML = profile.profile_username;
+        if(profile.follow_available){
+            follow_button.style.display = 'unset';
+            if(profile.currently_following){
+                follow_button.innerHTML = 'Unfollow';
+
+            }else {
+                follow_button.innerHTML = 'Follow';
+            }
+            follow_button.addEventListener('click', () => update_follow(creator_id));
+        }
+    })
+    window.scrollTo(0,0);
 }
