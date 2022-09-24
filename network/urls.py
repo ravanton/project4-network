85654d05eb8010f4fdd5a4ab2f4b0597671776f7
ravanton <antonrekhva@gmail.com>
2022-09-24 11:09:42 +0300

    add urls.py

--- network/urls.py
+++ network/urls.py
@@ -7,5 +7,11 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register")
+    path("register", views.register, name="register"),
+    path("save_post", views.save_post, name="save_post"),
+    path("load", views.load_posts, name="load_post"),
+    path("load/followed", views.load_followed_posts, name="load_followed_posts"),
+    path("profile/<int:user_id>/update_follow", views.update_follow, name="update_follow"),
+    path("profile/<int:user_id>", views.profile, name="profile"),
+    path("profile/<int:post_id>/update_like", views.update_like, name="update_like")
 ]
